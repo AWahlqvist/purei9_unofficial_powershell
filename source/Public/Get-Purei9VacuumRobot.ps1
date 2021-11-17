49bacf14a175af26983778cb39f8a610ef8e38a8
Anders Wahlqvist <anders.wahlqvist@hotmail.com>
2021-11-17 18:53:07 +0100

    Added more robot properties

--- source/Public/Get-Purei9VacuumRobot.ps1
+++ source/Public/Get-Purei9VacuumRobot.ps1
@@ -43,8 +43,53 @@ function Get-Purei9VacuumRobot {
             }
             $applianceInfo = InvokeApiRequest @invokeApiRequestSplat
         
+            $uriEnding = "/Appliances/$($appliance.pncId)"
+            $requestHash = GetApiRequestSplattingHash -UriEnding $uriEnding
+            $invokeApiRequestSplat.RequestSplattingHash = $requestHash
+            $applianceDetails = InvokeApiRequest @invokeApiRequestSplat
+            
+            $batteryLevel = switch ($applianceDetails.twin.properties.reported.batteryStatus) {
+                1 { "Dead" }
+                2 { "CriticalLow" }
+                3 { "Low" }
+                4 { "Medium" }
+                5 { "Normal" }
+                6 { "High" }
+                Default { "Unknown" }
+            }
+
+            $powerMode = switch ($applianceDetails.twin.properties.reported.powerMode) {
+                1 { "Low" }
+                2 { "Medium/Smart" }
+                3 { "High" }
+                Default { "Unknown" }
+            }
+
+            $robotStatus = switch ($applianceDetails.twin.properties.reported.robotStatus) {
+                 1 { "Cleaning" }
+                 2 { "Paused_Cleaning" }
+                 3 { "Spot_Cleaning" }
+                 4 { "Paused_Spot_Cleaning" }
+                 5 { "Return" }
+                 6 { "Paused_Return" }
+                 7 { "Return_for_Pitstop" }
+                 8 { "Paused_Return_for_Pitstop" }
+                 9 { "Charging" }
+                10 { "Sleeping" }
+                11 { "Error" }
+                12 { "Pitstop" }
+                13 { "Manual_Steering" }
+                14 { "Firmware_Upgrade" }
+                Default { "Unknown" }
+            }
+
             if ($applianceInfo.device -eq 'ROBOTIC_VACUUM_CLEANER') {
+                $appliance | Add-Member -MemberType NoteProperty -Name BatteryLevel -Value $batteryLevel
+                $appliance | Add-Member -MemberType NoteProperty -Name RobotStatus -Value $robotStatus
+                $appliance | Add-Member -MemberType NoteProperty -Name DustbinStatus -Value $applianceDetails.twin.properties.reported.dustbinStatus
+                $appliance | Add-Member -MemberType NoteProperty -Name PowerMode -Value $powerMode
                 $appliance | Add-Member -MemberType NoteProperty -Name info -Value $applianceInfo
+                $appliance | Add-Member -MemberType NoteProperty -Name details -Value $applianceDetails
                 $appliance
             }
         }
